ca2f16109d20e3b6d308a220495d581452c1c979
sixducks <82391600+sixducks@users.noreply.github.com>
2021-05-14 22:16:53 +0800

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int score;
+	scanf("%d", &score);
+	if (score<=100&&score>90)
+		printf("A");
+	else if (score<=90&&score > 80)
+	{
+		printf("B");
+	}
+	else if (score<=80&&score > 70)
+	{
+		printf("C");
+	}
+	else if (score<=70&&score > 60)
+	{
+		printf("D");
+	}
+	else if (score<=60)
+	{
+		printf("E");
+	}
+	return 0;
+}
